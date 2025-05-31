bc7b0a498595d203ceb144143c83635201e7adc1
Michael Korobkov <fpga-systems@yandex.ru>
2025-05-31 20:18:01 +0300

    initial release

---
+++ fs.tcl
@@ -0,0 +1,71 @@
+namespace eval ::fs {
+    lappend auto_path [file dirname [info script]]/XilinxTclStore/pkgIndex.tcl
+}
+
+
+proc ::fs::mem {} {
+    variable mems [get_cells -hier -filter "PRIMITIVE_GROUP	== BMEM"]
+
+    set net_width_port_a [list ]
+    set net_width_port_b [list ]
+    set mem_type [list ]
+    set mem_name [list ]
+    set bus_width [list ]
+    set mem_depth [list ]
+
+    foreach m $mems {
+        
+        set n [llength [get_nets -filter "TYPE == SIGNAL" -of_objects [get_pins -filter "BUS_NAME == DIADI"  -of [get_cells $m] ] ]]
+        if {[llength $n] == 0} {
+            lappend net_width_port_a "-"
+        } else {
+            lappend net_width_port_a $n
+        }
+
+        set n [llength [get_nets -filter "TYPE == SIGNAL" -of_objects [get_pins -filter "BUS_NAME == DIBDI"  -of [get_cells $m] ] ]]
+        if {[llength $n] == 0} {
+            lappend net_width_port_b "-"
+        } else {
+            lappend net_width_port_b $n
+        }
+
+        lappend mem_type [get_property REF_NAME $m]
+        lappend mem_name [get_property NAME     $m]
+
+        puts $m
+        
+        if {[get_property PRIMITIVE_SUBGROUP [get_cells $m]] eq "fifo"} {
+            lappend bus_width [lindex [get_property BUS_WIDTH [get_nets -filter "TYPE == SIGNAL" -of_objects [get_pins -filter {BUS_NAME == DI}  -of [get_cells $m] ] ] ] 0]
+            lappend mem_depth "?"
+        }
+
+        if {[get_property PRIMITIVE_SUBGROUP [get_cells $m]] eq "bram"} {
+            lappend bus_width [lindex [get_property BUS_WIDTH [get_nets -filter "TYPE == SIGNAL" -of_objects [get_pins -regexp -filter {BUS_NAME =~ DI.*DI}  -of [get_cells $m] ] ] ] 0]
+            lappend mem_depth [expr 2**[lindex [get_property BUS_WIDTH [get_nets -filter "TYPE == SIGNAL" -of_objects [get_pins -regexp -filter {BUS_NAME =~ ADDRA.*}  -of [get_cells $m] ] ] ] 0]]
+        }
+    }
+
+    set tbl [::tclapp::xilinx::designutils::prettyTable {TABLICHKA DLYA ANDRUHI}]
+    $tbl header [list Num BUS_WIDTH DINA DINB DEPTH TYPE  NAME]
+
+    for {set i 0} {$i < [llength $mems]} {incr i} {
+
+        $tbl addrow [list $i [lindex $bus_width $i] [lindex $net_width_port_a $i] [lindex $net_width_port_b $i] [lindex $mem_depth $i] [lindex $mem_type $i] [lindex $mem_name $i]]
+    }
+
+    $tbl print
+
+}
+
+
+proc ::fs::get_fmax {} {
+    variable fmax
+    
+    set slack [get_property SLACK [ get_timing_paths ]]
+    set clock [get_property STARTPOINT_CLOCK [ get_timing_paths ]]
+    set period [get_property PERIOD [get_clocks $clock]]
+
+    set fmax [format %.3f [expr 1.0/($period - $slack) * 1000.0]]
+   
+    return -code ok "Max frequency of design is $fmax MHz"
+}
